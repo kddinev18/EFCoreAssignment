e304beb6e54dbe93b4b06f5ae4cbb5539aad695a
Stefan Hristov <SHHristov20@codingburgas.bg>
2025-01-09 09:28:31 +0200

    Implement user model

---
+++ EFCoreAssignment/EFCoreAssignment.Data/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFCoreAssignment.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public DateOnly DateCreatead { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+    }
+}
