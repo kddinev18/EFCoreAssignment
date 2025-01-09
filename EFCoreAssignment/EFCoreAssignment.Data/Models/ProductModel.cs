2ecbb877dfb2e935a9cd6b6ca76ee9676ac15754
Stefan Hristov <SHHristov20@codingburgas.bg>
2025-01-09 09:44:01 +0200

    Implement product model

---
+++ EFCoreAssignment/EFCoreAssignment.Data/Models/ProductModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreAssignment.Data.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public string Category { get; set; } = null!;
+
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+
+    }
+}
--- EFCoreAssignment/EFCoreAssignment.Data/Models/UserModel.cs
+++ EFCoreAssignment/EFCoreAssignment.Data/Models/UserModel.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EFCoreAssignment.Data.Models
 {
     public class User
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
