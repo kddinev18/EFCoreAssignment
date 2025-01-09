ca5c7743e55d9937431cf2ffcbe0705b145f49cc
Stefan Hristov <SHHristov20@codingburgas.bg>
2025-01-09 09:59:55 +0200

    Implemet order model

---
+++ EFCoreAssignment/EFCoreAssignment.Data/Models/OrderModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreAssignment.Data.Models
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public DateOnly DateCreated { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal TotalAmount { get; set; }
+
+        [Required]
+        public string Status { get; set; } = null!;
+    }
+}
